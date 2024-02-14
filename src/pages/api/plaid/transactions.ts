f38fd323e7d9a5f28a3ea66e53bda17f2894bb53
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2024-02-13 22:28:53 -0800

    Installed Plaid API and imported the environment keys

---
+++ src/pages/api/plaid/transactions.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { Configuration, PlaidApi, Products, PlaidEnvironments } from "plaid";
+// const util = require("util");
+// const { v4: uuidv4 } = require("uuid");
+// const bodyParser = require("body-parser");
+// const moment = require("moment");
+// const cors = require("cors");
+
+// Assigning the environment variables to the variables
+const APP_PORT = process.env.APP_PORT || 8000;
+const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
+const PLAID_SECRET = process.env.PLAID_SECRET;
+const PLAID_ENV = process.env.PLAID_ENV || "sandbox";
+
+const PLAID_PRODUCTS = (
+  process.env.PLAID_PRODUCTS || Products.Transactions
+).split(",");
+
+const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || "US").split(
+  ","
+);
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments[PLAID_ENV],
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": PLAID_CLIENT_ID,
+      "PLAID-SECRET": PLAID_SECRET,
+      "Plaid-Version": "2020-09-14",
+    },
+  },
+});
