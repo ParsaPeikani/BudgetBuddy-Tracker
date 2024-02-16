6037f6b61d0cadd4ac9d58a2a1f0e5da536fa197
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2024-02-15 21:38:02 -0800

    Added Custom PIE chart from google-charts without data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import Navbar from "@/components/navbar/navbar";
+import PieChartComponent from "@/components/charts/donut";
 
 export default function Dashboard() {
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
+      <PieChartComponent />
     </div>
   );
 }
