087a96d1806b4070189b175df4bc8c8a4f8085c7
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2024-03-03 14:24:34 -0800

    Added checkbox for selecting the rows

--- src/components/custom-table/data-table.tsx
+++ src/components/custom-table/data-table.tsx
@@ -48,6 +48,7 @@ export function DataTable<TData, TValue>({
   );
   const [columnVisibility, setColumnVisibility] =
     React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
 
   const table = useReactTable({
     data,
@@ -59,15 +60,21 @@ export function DataTable<TData, TValue>({
     onColumnFiltersChange: setColumnFilters,
     getFilteredRowModel: getFilteredRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
     state: {
       sorting,
       columnFilters,
       columnVisibility,
+      rowSelection,
     },
   });
 
   return (
     <div>
+      <div className="flex-1 text-sm text-muted-foreground">
+        {table.getFilteredSelectedRowModel().rows.length} of{" "}
+        {table.getFilteredRowModel().rows.length} row(s) selected.
+      </div>
       <div className="flex items-center py-4">
         <Input
           placeholder="Filter emails..."
