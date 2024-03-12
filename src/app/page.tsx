0283f37ef806a68f7f1a4d857f72d1fd0375a6d5
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2024-03-11 23:49:39 -0700

    Added a little bit of styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,17 +17,17 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-deep-blue text-white space-y-8">
       <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to BudgetPro</h1>
+        <h1 className="text-4xl font-bold mb-4">Welcome to BudgetPro Parsa</h1>
         <p className="max-w-md text-lg">
           Your personal finance assistant. Manage your budgets, track your
-          expenses, and make smarter financial decisions with BudgetPro.
+          expenses, and make smarter financial decisions with BudgetPro :)
         </p>
       </div>
 
       <Link
         href="/dashboard"
         onClick={handleClick}
-        className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow hover:bg-blue-600 transition duration-300"
+        className="px-6 py-3 bg-transparent text-white font-semibold rounded-lg shadow hover:bg-white hover:text-black transition duration-300 border-2 border-white"
       >
         Go to Dashboard
       </Link>
@@ -36,7 +36,7 @@ export default function Home() {
         <div className="text-center">
           <Link
             href="/sign-up"
-            className="px-6 py-3 bg-green-500 text-white font-semibold rounded-lg shadow hover:bg-green-600 transition duration-300"
+            className="px-6 py-3 bg-transparent text-white font-semibold rounded-lg shadow hover:bg-white hover:text-black transition duration-300 border-2 border-white"
           >
             Sign in or Sign up
           </Link>
