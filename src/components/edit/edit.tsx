9af6afde492d262151bf8db165c25c6c338217ca
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2024-04-01 12:15:57 -0700

    The edit modal grabs the chagned data

--- src/components/edit/edit.tsx
+++ src/components/edit/edit.tsx
@@ -10,10 +10,7 @@ import {
 } from "@/components/ui/dialog";
 import { Switch } from "@/components/ui/switch";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Checkbox } from "@/components/ui/checkbox";
 
-////////////////////////////
 import { zodResolver } from "@hookform/resolvers/zod";
 import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
@@ -25,7 +22,6 @@ import { Calendar } from "@/components/ui/calendar";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -37,14 +33,13 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { toast } from "@/components/ui/use-toast";
-import { useEffect, useState } from "react";
-/////////////////////////////
+import { useState } from "react";
 
 // Define your form validation schema using Zod
 const transactionSchema = z.object({
   name: z.string(),
   amount: z.string(),
-  // date: z.date(),
+  date: z.date(),
   category: z.string(),
   verified: z.boolean(),
 });
@@ -55,17 +50,13 @@ export function Edit({ transaction }: { transaction: any }) {
     defaultValues: {
       name: transaction.transaction || "Unknown",
       amount: transaction.amount.toString(),
-      // date: new Date(transaction.date),
+      date: new Date(transaction.date),
       category: transaction.category,
       verified: !transaction.verified,
     },
   });
 
   const [checkedBox, setCheckedBox] = useState(!transaction.verified);
-
-  // useEffect(() => {
-  //   setValue("date", new Date(transaction.date));
-  // }, [transaction.date, setValue]);
 
   const onSubmit = (data: any) => {
     console.log("These are the date: ", data);
@@ -74,7 +65,6 @@ export function Edit({ transaction }: { transaction: any }) {
   const onError = (errors: any) => {
     console.error("Form submission errors:", errors);
   };
-  // console.log("This is the transaction: ", transaction);
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -99,7 +89,6 @@ export function Edit({ transaction }: { transaction: any }) {
             className="w-full space-y-6"
           >
             <div>
-              <h3 className="mb-4 text-lg font-medium">Email Notifications</h3>
               <div className="space-y-4">
                 <FormField
                   control={form.control}
@@ -135,6 +124,48 @@ export function Edit({ transaction }: { transaction: any }) {
                           {...field}
                         />
                       </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="date"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col md:flex-row items-center justify-between space-y-2 md:space-y-0 w-full">
+                      <FormLabel>Date</FormLabel>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant={"outline"}
+                              className={cn(
+                                "w-[240px] pl-3 text-left font-normal",
+                                !field.value && "text-muted-foreground"
+                              )}
+                            >
+                              {field.value ? (
+                                format(field.value, "PPP")
+                              ) : (
+                                <span>Pick a date</span>
+                              )}
+                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0" align="start">
+                          <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={field.onChange}
+                            disabled={(date) =>
+                              date > new Date() || date < new Date("1900-01-01")
+                            }
+                            initialFocus
+                          />
+                        </PopoverContent>
+                      </Popover>
+                      <FormMessage />
                     </FormItem>
                   )}
                 />
@@ -178,11 +209,7 @@ export function Edit({ transaction }: { transaction: any }) {
               </div>
             </div>
             <DialogFooter>
-              <Button
-                variant="outline"
-                type="submit"
-                // onClick={() => console.log("Submitting the form")}
-              >
+              <Button variant="outline" type="submit">
                 Save changes
               </Button>
             </DialogFooter>
