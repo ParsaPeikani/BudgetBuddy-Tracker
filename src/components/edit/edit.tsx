fc1a28af1daa2998b7ad83c0c0349808cc490702
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2024-04-01 12:00:02 -0700

    Refactored the edit functionality using the Form field of shadcn ui

--- src/components/edit/edit.tsx
+++ src/components/edit/edit.tsx
@@ -8,6 +8,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Switch } from "@/components/ui/switch";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
@@ -42,19 +43,19 @@ import { useEffect, useState } from "react";
 // Define your form validation schema using Zod
 const transactionSchema = z.object({
   name: z.string(),
-  amount: z.number(),
-  date: z.date(),
+  amount: z.string(),
+  // date: z.date(),
   category: z.string(),
   verified: z.boolean(),
 });
 
 export function Edit({ transaction }: { transaction: any }) {
-  const { control, handleSubmit, setValue } = useForm({
+  const form = useForm({
     resolver: zodResolver(transactionSchema),
     defaultValues: {
       name: transaction.transaction || "Unknown",
-      amount: transaction.amount,
-      date: new Date(transaction.date),
+      amount: transaction.amount.toString(),
+      // date: new Date(transaction.date),
       category: transaction.category,
       verified: !transaction.verified,
     },
@@ -62,18 +63,18 @@ export function Edit({ transaction }: { transaction: any }) {
 
   const [checkedBox, setCheckedBox] = useState(!transaction.verified);
 
-  useEffect(() => {
-    setValue("date", new Date(transaction.date));
-  }, [transaction.date, setValue]);
+  // useEffect(() => {
+  //   setValue("date", new Date(transaction.date));
+  // }, [transaction.date, setValue]);
 
   const onSubmit = (data: any) => {
-    console.log(data);
+    console.log("These are the date: ", data);
     // Proceed with your submission logic here...
   };
   const onError = (errors: any) => {
     console.error("Form submission errors:", errors);
   };
-  console.log("This is the transaction: ", transaction);
+  // console.log("This is the transaction: ", transaction);
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -92,139 +93,101 @@ export function Edit({ transaction }: { transaction: any }) {
             you&apos;re done.
           </DialogDescription>
         </DialogHeader>
-        <form
-          onSubmit={handleSubmit(onSubmit, onError)}
-          className="grid gap-4 py-4"
-        >
-          <div className="grid gap-4 py-4">
-            <Controller
-              name="name"
-              control={control}
-              render={({ field }) => (
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="name" className="text-right">
-                    Name
-                  </Label>
-                  <Input
-                    {...field}
-                    id="name"
-                    placeholder="Enter Transaction Name"
-                    className="col-span-3"
-                  />
-                </div>
-              )}
-            />
-            <Controller
-              name="amount"
-              control={control}
-              render={({ field }) => (
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="amount" className="text-right">
-                    Amount
-                  </Label>
-                  <Input
-                    {...field}
-                    onChange={(e) => field.onChange(Number(e.target.value))} // Convert string to number here
-                    id="amount"
-                    placeholder="Enter Transaction Amount"
-                    type="number"
-                    className="col-span-3"
-                  />
-                </div>
-              )}
-            />
-            <Controller
-              name="date"
-              control={control}
-              render={({ field }) => (
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="date" className="text-right">
-                    Date
-                  </Label>
-                  <div className="col-span-3 flex">
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <Button
-                          variant="outline"
-                          className="flex w-full justify-between bg-black text-white hover:bg-primary hover:text-white"
-                        >
-                          {field.value
-                            ? format(field.value, "PPP")
-                            : "Pick a date"}
-                          <CalendarIcon className="ml-2 h-4 w-4 text-white" />
-                        </Button>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-auto p-0" align="start">
-                        <Calendar
-                          mode="single"
-                          selected={field.value}
-                          onSelect={(date) => {
-                            field.onChange(date);
-                          }}
-                          disabled={(date) =>
-                            date > new Date() || date < new Date("1900-01-01")
-                          }
-                          initialFocus
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-6"
+          >
+            <div>
+              <h3 className="mb-4 text-lg font-medium">Email Notifications</h3>
+              <div className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col md:flex-row items-center md:justify-between space-y-2 md:space-y-0 w-full">
+                      <div className="md:flex-1">
+                        <FormLabel className="text-base">Name</FormLabel>
+                      </div>
+                      <FormControl className="flex-grow">
+                        <Input
+                          className="ml-8 w-full"
+                          placeholder="Enter Transaction Name"
+                          {...field}
                         />
-                      </PopoverContent>
-                    </Popover>
-                  </div>
-                </div>
-              )}
-            />
-            <Controller
-              name="category"
-              control={control}
-              render={({ field }) => (
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="category" className="text-right">
-                    Categories
-                  </Label>
-                  <Input
-                    {...field}
-                    id="category"
-                    placeholder="Enter Transaction Category"
-                    className="col-span-3"
-                  />
-                </div>
-              )}
-            />
-            <Controller
-              name="verified"
-              control={control}
-              render={({ field: { onChange, value, ref } }) => {
-                console.log("this is the value", value); // This will log 'true' or 'false'
-                return (
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="verified" className="text-right">
-                      Verified
-                    </Label>
-                    <div className="col-span-3 flex items-center">
-                      <Input
-                        id="category"
-                        type="checkbox"
-                        defaultChecked={value}
-                        onChange={(e) => {
-                          onChange(e.target.checked);
-                        }}
-                      />
-                    </div>
-                  </div>
-                );
-              }}
-            />
-          </div>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="amount"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col md:flex-row items-center justify-between space-y-2 md:space-y-0 w-full">
+                      <div className="flex-1">
+                        <FormLabel className="text-base">Amount</FormLabel>
+                      </div>
+                      <FormControl className="md:w-48">
+                        <Input
+                          className="w-full md:ml-auto"
+                          type="number"
+                          placeholder="Enter Transaction Amount"
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
 
-          <DialogFooter>
-            <Button
-              variant="outline"
-              type="submit"
-              // onClick={() => console.log("Submitting the form")}
-            >
-              Save changes
-            </Button>
-          </DialogFooter>
-        </form>
+                <FormField
+                  control={form.control}
+                  name="category"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col md:flex-row items-center md:justify-between space-y-2 md:space-y-0 w-full">
+                      <div className="md:flex-1">
+                        <FormLabel className="text-base">Category</FormLabel>
+                      </div>
+                      <FormControl className="flex-grow">
+                        <Input
+                          className="ml-8 w-full"
+                          placeholder="Enter Transaction Name"
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="verified"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                      <div className="space-y-0.5">
+                        <FormLabel className="text-base">Verified</FormLabel>
+                      </div>
+                      <FormControl>
+                        <Switch
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                          aria-readonly
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button
+                variant="outline"
+                type="submit"
+                // onClick={() => console.log("Submitting the form")}
+              >
+                Save changes
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
---
+++ src/components/edit/edit2.tsx
@@ -0,0 +1,240 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Checkbox } from "@/components/ui/checkbox";
+
+////////////////////////////
+import { zodResolver } from "@hookform/resolvers/zod";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { useForm, Controller } from "react-hook-form";
+import { z } from "zod";
+
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { toast } from "@/components/ui/use-toast";
+import { useEffect, useState } from "react";
+/////////////////////////////
+
+// Define your form validation schema using Zod
+const transactionSchema = z.object({
+  name: z.string(),
+  amount: z.number(),
+  date: z.date(),
+  category: z.string(),
+  verified: z.boolean(),
+});
+
+export function Edit({ transaction }: { transaction: any }) {
+  const { control, handleSubmit, setValue } = useForm({
+    resolver: zodResolver(transactionSchema),
+    defaultValues: {
+      name: transaction.transaction || "Unknown",
+      amount: transaction.amount,
+      date: new Date(transaction.date),
+      category: transaction.category,
+      verified: !transaction.verified,
+    },
+  });
+
+  const [checkedBox, setCheckedBox] = useState(!transaction.verified);
+
+  useEffect(() => {
+    setValue("date", new Date(transaction.date));
+  }, [transaction.date, setValue]);
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    // Proceed with your submission logic here...
+  };
+  const onError = (errors: any) => {
+    console.error("Form submission errors:", errors);
+  };
+  console.log("This is the transaction: ", transaction);
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex bg-black border-none text-white font-normal "
+        >
+          Edit
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle className="text-center">Edit Transaction</DialogTitle>
+          <DialogDescription className="text-center">
+            Make changes to your specific transaction here. Click save when
+            you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <form
+          onSubmit={handleSubmit(onSubmit, onError)}
+          className="grid gap-4 py-4"
+        >
+          <div className="grid gap-4 py-4">
+            <Controller
+              name="name"
+              control={control}
+              render={({ field }) => (
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="name" className="text-right">
+                    Name
+                  </Label>
+                  <Input
+                    {...field}
+                    id="name"
+                    placeholder="Enter Transaction Name"
+                    className="col-span-3"
+                  />
+                </div>
+              )}
+            />
+            <Controller
+              name="amount"
+              control={control}
+              render={({ field }) => (
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="amount" className="text-right">
+                    Amount
+                  </Label>
+                  <Input
+                    {...field}
+                    onChange={(e) => field.onChange(Number(e.target.value))} // Convert string to number here
+                    id="amount"
+                    placeholder="Enter Transaction Amount"
+                    type="number"
+                    className="col-span-3"
+                  />
+                </div>
+              )}
+            />
+            <Controller
+              name="date"
+              control={control}
+              render={({ field }) => (
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="date" className="text-right">
+                    Date
+                  </Label>
+                  <div className="col-span-3 flex">
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant="outline"
+                          className="flex w-full justify-between bg-black text-white hover:bg-primary hover:text-white"
+                        >
+                          {field.value
+                            ? format(field.value, "PPP")
+                            : "Pick a date"}
+                          <CalendarIcon className="ml-2 h-4 w-4 text-white" />
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={(date) => {
+                            field.onChange(date);
+                          }}
+                          disabled={(date) =>
+                            date > new Date() || date < new Date("1900-01-01")
+                          }
+                          initialFocus
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </div>
+                </div>
+              )}
+            />
+            <Controller
+              name="category"
+              control={control}
+              render={({ field }) => (
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="category" className="text-right">
+                    Categories
+                  </Label>
+                  <Input
+                    {...field}
+                    id="category"
+                    placeholder="Enter Transaction Category"
+                    className="col-span-3"
+                  />
+                </div>
+              )}
+            />
+            <Controller
+              name="verified"
+              control={control}
+              render={({ field: { onChange, value, ref } }) => {
+                console.log("this is the value", value); // This will log 'true' or 'false'
+                return (
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="verified" className="text-right">
+                      Verified
+                    </Label>
+                    <div className="col-span-3 flex items-center">
+                      <Checkbox
+                        id="verified"
+                        // checked={value}
+                        defaultChecked={value}
+                        onChange={(e) => {
+                          onChange(e.target.checked);
+                        }}
+                      />
+                      {/* <input
+                        id="category"
+                        className="w-10 h-10 border-2 border-white-800 rounded-sm bg-primary checked:bg-black checked:border-0"
+                        type="checkbox"
+                        defaultChecked={value}
+                        onChange={(e) => {
+                          onChange(e.target.checked);
+                        }}
+                      /> */}
+                    </div>
+                  </div>
+                );
+              }}
+            />
+          </div>
+
+          <DialogFooter>
+            <Button
+              variant="outline"
+              type="submit"
+              // onClick={() => console.log("Submitting the form")}
+            >
+              Save changes
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
