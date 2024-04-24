cf15c085b78efab1cb3d936b69d7ffd37830f81e
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2024-04-24 08:27:27 -0700

    updated the balance api end point for fetching data from plaid

--- src/pages/api/plaid/balance.ts
+++ src/pages/api/plaid/balance.ts
@@ -2,13 +2,12 @@
 
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Configuration, PlaidApi, Products, PlaidEnvironments } from "plaid";
-import Transaction from "@/Models/transaction";
-import TDTransaction from "@/Models/tdTransactions";
 import connectDB from "@/pages/lib/connectDB";
+import Balance from "@/Models/balance";
 
 type ResponseData = {
   message: string;
-  latest_transactions: Transaction[];
+  balance: any;
 };
 
 // Assigning the environment variables to the variables
@@ -16,15 +15,11 @@ const APP_PORT = process.env.APP_PORT || 8000;
 const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
 const PLAID_ENV = process.env.PLAID_ENV || "sandbox";
-const ACCESS_TOKEN = process.env.PLAID_ACCESS_TOKEN;
+const ACCESS_TOKEN = process.env.PLAID_TD_ACCESS_TOKEN;
 
 const PLAID_PRODUCTS = (
   process.env.PLAID_PRODUCTS || Products.Transactions
 ).split(",");
-
-const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || "US").split(
-  ","
-);
 
 const configuration = new Configuration({
   basePath: PlaidEnvironments[PLAID_ENV],
@@ -50,25 +45,37 @@ export default async function handler(
   console.log("hello we are getting here");
   await connectDB();
   Promise.resolve().then(async function () {
-    // Set cursor to empty to receive all historical updates
-    let cursor = null;
-
-    // New transaction updates since "cursor"
-    let added: any = [];
-    let modified: any = [];
-    // Removed transaction ids
-    let removed: any = [];
-    let hasMore = true;
     // Iterate through each page of new transaction updates for item
     try {
       const request = {
         access_token: ACCESS_TOKEN!,
       };
       const response = await client.accountsBalanceGet(request);
-      const data = response.data;
       const accounts = response.data.accounts;
 
-      res.status(200).json({ message: "Success", balance: response.data });
+      // Uncomment the following code for development purposes
+      // Create a new balance object
+      //   for (const account of accounts) {
+      //     const balance = new Balance({
+      //       account: {
+      //         accountId: account.account_id,
+      //         balances: {
+      //           available: account.balances.available,
+      //           current: account.balances.current,
+      //           iso_currency_code: account.balances.iso_currency_code,ResponseData
+      //         },
+      //         mask: account.mask,
+      //         name: account.name,
+      //         subtype: account.subtype,
+      //       },
+      //     });
+      //     await balance.save();
+      //   }
+
+      res.status(200).json({
+        message: "Success Saving the balances in MongoDB",
+        balance: response.data,
+      });
     } catch (error: any) {
       // Send an error response back to the client
       console.error("There was an error!", error);
