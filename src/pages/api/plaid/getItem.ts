79207c80c6f0b1ad342b485effc89fbc574def5c
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2024-05-14 07:12:48 -0700

    Added the getItem endpoint for plaid

--- src/pages/api/plaid/getItem.ts
+++ src/pages/api/plaid/getItem.ts
@@ -1,22 +1,13 @@
 "use server";
-// Plaid Env Variables
 
-import { Configuration, PlaidApi, Products, PlaidEnvironments } from "plaid";
+// Plaid Env Variables
+import { Configuration, PlaidApi, PlaidEnvironments } from "plaid";
 
 // Assigning the environment variables to the variables
-const APP_PORT = process.env.APP_PORT || 8000;
 const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
 const PLAID_ENV = process.env.PLAID_ENV || "sandbox";
 const ACCESS_TOKEN = process.env.PLAID_CIBC_ACCESS_TOKEN;
-
-const PLAID_PRODUCTS = (
-  process.env.PLAID_PRODUCTS || Products.Transactions
-).split(",");
-
-const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || "US").split(
-  ","
-);
 
 const configuration = new Configuration({
   basePath: PlaidEnvironments[PLAID_ENV],
@@ -31,22 +22,19 @@ const configuration = new Configuration({
 
 const client = new PlaidApi(configuration);
 
-// /pages/api/transactions.js
-import connectDB from "@/pages/lib/connectDB";
-import CIBCTransaction from "@/Models/cibcTransactions";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await connectDB();
-
-  if (req.method === "GET") {
+  if (req.method === "PUT") {
     try {
-      console.log("we are getting here");
+      const response = await client.itemGet({ access_token: ACCESS_TOKEN! });
+      const item = response.data.item;
       res.status(200).json({
         message: `Webhook URL is working`,
+        item,
       });
     } catch (error) {
       res.status(500).json({ message: "Error Deleting Transactions" });
